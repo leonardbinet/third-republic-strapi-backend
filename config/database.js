349c45109fdb8cd985905080ae5f3bf690d8f01c
Leonard Binet <lbinet@alkemics.com>
2020-09-27 20:19:55 +0200

    dev vs prod databases setup

--- config/database.js
+++ config/database.js
@@ -1,15 +1,28 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  // in development environment use local sqllite
+  defaultConnection: env("NODE_ENV", "development"),
   connections: {
-    default: {
-      connector: 'bookshelf',
+    development: {
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
         useNullAsDefault: true,
       },
     },
+  },
+  // in production use remote postgres
+  production: {
+    connector: "bookshelf",
+    settings: {
+      client: "postgres",
+      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+      database: env("DATABASE_NAME"),
+      username: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+    },
+    options: {},
   },
 });
